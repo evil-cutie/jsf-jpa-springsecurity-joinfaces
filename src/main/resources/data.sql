ece60205846a411a810aa15f29952b36d7cbaa95
evil-cutie <109818538+evil-cutie@users.noreply.github.com>
2025-08-06 00:40:48 +0700

    fixed routes in data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,6 +2,7 @@
 INSERT INTO users (password, username, user_email, active) VALUES('$2a$12$Oots1YAVyUk.etOo9WKaH.n97qrr0thU6fLs2/AdiMwLpyafefGnm', 'panda', 'panda@gmail.com', true);
 INSERT INTO users (password, username, user_email, active) VALUES ('$2a$12$Oots1YAVyUk.etOo9WKaH.n97qrr0thU6fLs2/AdiMwLpyafefGnm', 'not_admin', 'notadmin@gmail.com', true);
 INSERT INTO users (password, username, user_email, active) VALUES('$2a$12$Oots1YAVyUk.etOo9WKaH.n97qrr0thU6fLs2/AdiMwLpyafefGnm', 'catlover', 'hunter2@gmail.com', true);
+
 --routes
 INSERT INTO routes (route_url) VALUES ('/index.xhtml');
 INSERT INTO routes (route_url) VALUES ('/403.xhtml');
@@ -16,10 +17,12 @@ INSERT INTO routes (route_url) VALUES ('/images/favicon.ico');
 INSERT INTO routes (route_url) VALUES ('/history.xhtml');
 INSERT INTO routes (route_url) VALUES ('/api/users/register');
 INSERT INTO routes (route_url) VALUES ('/redirect.xhtml');
+
 --roles
 INSERT INTO roles (role_name) VALUES ('ROLE_ADMIN');
 INSERT INTO roles (role_name) VALUES ('ROLE_USER');
 INSERT INTO roles (role_name) VALUES ('ROLE_GUEST');
+
 --animals
 INSERT INTO animals (animal_type) VALUES ('cat');
 INSERT INTO animals (animal_type) VALUES ('dog');
@@ -27,6 +30,7 @@ INSERT INTO animals (animal_type) VALUES ('parrot');
 INSERT INTO animals (animal_type) VALUES ('fish');
 INSERT INTO animals (animal_type) VALUES ('hamster');
 INSERT INTO animals (animal_type) VALUES ('guinea pig');
+
 --cities
 INSERT INTO cities (city_name) VALUES ('Tel Aviv');
 INSERT INTO cities (city_name) VALUES ('Jerusalem');
@@ -40,40 +44,47 @@ INSERT INTO cities (city_name) VALUES ('Bat Yam');
 INSERT INTO cities (city_name) VALUES ('Ramat Gan');
 INSERT INTO cities (city_name) VALUES ('Herzliya');
 INSERT INTO cities (city_name) VALUES ('Rehovot');
+
 --mapping of routes and roles
+--ROLE_ADMIN
 INSERT INTO route_role (route_id, role_id) VALUES (1, 1);
 INSERT INTO route_role (route_id, role_id) VALUES (2, 1);
 INSERT INTO route_role (route_id, role_id) VALUES (3, 1);
 INSERT INTO route_role (route_id, role_id) VALUES (4, 1);
 INSERT INTO route_role (route_id, role_id) VALUES (5, 1);
 INSERT INTO route_role (route_id, role_id) VALUES (6, 1);
-INSERT INTO route_role (route_id, role_id) VALUES (7, 1);
 INSERT INTO route_role (route_id, role_id) VALUES (8, 1);
 INSERT INTO route_role (route_id, role_id) VALUES (9, 1);
+INSERT INTO route_role (route_id, role_id) VALUES (10, 1);
+INSERT INTO route_role (route_id, role_id) VALUES (11, 1);
+INSERT INTO route_role (route_id, role_id) VALUES (13, 1);
+--ROLE_USER
 INSERT INTO route_role (route_id, role_id) VALUES (1, 2);
 INSERT INTO route_role (route_id, role_id) VALUES (2, 2);
 INSERT INTO route_role (route_id, role_id) VALUES (3, 2);
 INSERT INTO route_role (route_id, role_id) VALUES (4, 2);
 INSERT INTO route_role (route_id, role_id) VALUES (5, 2);
 INSERT INTO route_role (route_id, role_id) VALUES (6, 2);
-INSERT INTO route_role (route_id, role_id) VALUES (7, 2);
 INSERT INTO route_role (route_id, role_id) VALUES (9, 2);
+INSERT INTO route_role (route_id, role_id) VALUES (10, 2);
+INSERT INTO route_role (route_id, role_id) VALUES (11, 2);
+INSERT INTO route_role (route_id, role_id) VALUES (13, 2);
+--ROLE_GUEST
 INSERT INTO route_role (route_id, role_id) VALUES (1, 3);
 INSERT INTO route_role (route_id, role_id) VALUES (2, 3);
 INSERT INTO route_role (route_id, role_id) VALUES (3, 3);
 INSERT INTO route_role (route_id, role_id) VALUES (4, 3);
 INSERT INTO route_role (route_id, role_id) VALUES (5, 3);
 INSERT INTO route_role (route_id, role_id) VALUES (7, 3);
-INSERT INTO route_role (route_id, role_id) VALUES (9, 3);
-INSERT INTO route_role (route_id, role_id) VALUES (10, 1);
-INSERT INTO route_role (route_id, role_id) VALUES (10, 2);
-INSERT INTO route_role (route_id, role_id) VALUES (11, 3);
+INSERT INTO route_role (route_id, role_id) VALUES (10, 3);
 INSERT INTO route_role (route_id, role_id) VALUES (12, 3);
-INSERT INTO route_role (route_id, role_id) VALUES (12, 2);
-INSERT INTO route_role (route_id, role_id) VALUES (12, 1);
+INSERT INTO route_role (route_id, role_id) VALUES (13, 3);
+
 --mapping of users and roles
 INSERT INTO user_role (user_id, role_id) VALUES (1, 1);
 INSERT INTO user_role (user_id, role_id) VALUES (2, 2);
+INSERT INTO user_role (user_id, role_id) VALUES (3, 2);
+
 --listings
 INSERT INTO listings (description, listing_image, listing_title, active, city_id, animal_id, user_id, phone) VALUES ('Looking for a new home for my neighbours cat due to relocation. Fluff is a gentle and friendly cat who likes to sleep a lot :)', '/images/cat1.png', 'Fluff is looking for a new family', true, 2, 1, 1,'+97255000000');
 INSERT INTO listings (description, listing_image, listing_title, active, city_id, animal_id, user_id, phone) VALUES ('Unfortunately, the owner of Whiskers has cat allergy so we have to find for him a new home', '/images/cat2.png', 'Urgent! Whiskers needs a new home',true, 5,1, 2, '+97255000000');
